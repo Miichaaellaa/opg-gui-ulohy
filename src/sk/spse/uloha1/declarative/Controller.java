f662c584e8bb69c777dcced2451419a7ffb877c0
Štofanová <stofanovamichaela@gmail.com>
2026-02-16 11:26:03 +0100

    Declarative Application 1

--- src/sk/spse/uloha1/declarative/Controller.java
+++ src/sk/spse/uloha1/declarative/Controller.java
@@ -2,12 +2,15 @@ package sk.spse.uloha1.declarative;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
  */
 public class Controller {
 
+    public TextField cField;
+    public TextField fField;
     private int counter = 0;
 
     @FXML
@@ -17,5 +20,27 @@ public class Controller {
     private void incrementCounter() {
         counter++;
         counterField.setText(String.valueOf(counter));
+    }
+
+    @FXML
+    public void convertCtoF() {
+        try {
+            double c = Double.parseDouble(cField.getText());
+            double f = (c * 9 / 5) + 32;
+            fField.setText(String.valueOf(f));
+        } catch (NumberFormatException e) {
+            fField.setText("Neplatné číslo");
+        }
+    }
+
+    @FXML
+    public void convertFtoC() {
+        try {
+            double f = Double.parseDouble(fField.getText());
+            double c = (f - 32) * 5 / 9;
+            cField.setText(String.valueOf(c));
+        } catch (NumberFormatException e) {
+            cField.setText("Neplatné číslo");
+        }
     }
 }
--- src/sk/spse/uloha1/procedural/Application.java
+++ src/sk/spse/uloha1/procedural/Application.java
@@ -1,11 +1,13 @@
 package sk.spse.uloha1.procedural;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 ///
@@ -16,28 +18,56 @@ import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
 
-    private int counter = 0;
+    private TextField cField;
+    private TextField fField;
+
+    public void convertCtoF(KeyEvent keyEvent) {
+        try {
+            double c = Double.parseDouble(cField.getText());
+            double f = (c * 9.0 / 5.0) + 32.0;
+            fField.setText(String.valueOf(f));
+        } catch (NumberFormatException e) {
+            fField.setText("");
+        }
+    }
+
+    public void convertFtoC(KeyEvent keyEvent) {
+        try {
+            double f = Double.parseDouble(fField.getText());
+            double c = (f - 32.0) * 5.0 / 9.0;
+            cField.setText(String.valueOf(c));
+        } catch (NumberFormatException e) {
+            cField.setText("");
+        }
+    }
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Počítadlo:");
 
-        TextField textField = new TextField("0");
+        Label cLabel = new Label("Stupne Celsius         ");
+        cField = new TextField();
+        cField.setOnKeyTyped(this::convertCtoF);
+        Label cUnit = new Label("°C");
 
-        Button button = new Button("Plus 1");
+        Label fLabel = new Label("Stupne Fahrenheita");
+        fField = new TextField();
+        fField.setOnKeyTyped(this::convertFtoC);
+        Label fUnit = new Label("°F");
 
-        button.setOnAction(e -> {
-            counter++;
-            textField.setText(String.valueOf(counter));
-        });
+        HBox cel = new HBox(20, cLabel, cField, cUnit);
+        HBox fah = new HBox(20, fLabel, fField, fUnit);
+        VBox root = new VBox(20, cel, fah);
 
-        HBox root = new HBox(20, label, textField, button);
         root.setPadding(new Insets(10));
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("Procedural Application 1");
+        stage.setTitle("Declarative Application 1");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
